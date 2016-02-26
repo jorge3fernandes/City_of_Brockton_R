367bd22e442bda4ecbcca251ba46a2fc10296540
jorge3fernandes <jorge3fernandes@gmail.com>
2016-02-26 16:04:23 -0500

    w

--- PDFtoText.R
+++ PDFtoText.R
@@ -22,10 +22,31 @@ text <- readLines(filetxt)
 
 #parsing text
 text <- readLines("testpdf_pdfbox.txt")
+
+#marking where to split
+
+for (i in seq_along(text)){ 
+      
+     if (str_detect(text[i],'                [[:digit:]]{4}')) { 
+           text[i] <- paste0("CALL BEGINS HERE",text[i] )
+     }
+
+}
+
+
 txt <- str_c(text, collapse = "\n")
 
-txtparts <- unlist(str_split(txt, '                [[:digit:]]{4}'))
-cat()
+#split the text by calls
+txtparts <- unlist(str_split(txt, "CALL BEGINS HERE"))
+
+#trim leading and trailing white spaces
+
+time <- str_trim(str_extract(txtparts, "                [[:digit:]]{4}"))
+date <- str_extract(txtparts[1], "\\d{2}/\\d{2}/\\d{4}")
+Call_taker <- str_extract(txtparts, "Call Taker:.*\n")
+address <- str_extract(txtparts, "Location/Address:.*\n")
+
+
 
 
 
