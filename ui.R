b2d2269103358d78d50d57a7133579ce4489c85c
jorge3fernandes <jorge3fernandes@gmail.com>
2016-03-30 21:54:44 -0400

    delete

--- ui.R
+++ ui.R
@@ -3,7 +3,16 @@ library(leaflet)
 ## leafletOutput is used at the ui side to display the rendered map.
 
 shinyUI(fluidPage(
-  leafletOutput("mymap", width = "100%", height = 1200)
   
+  titlePanel("Filters"),
   
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("range", "Range:",
+                  min = 1, max = 1000, value = c(200,500))
+    )
+  ),
+  mainPanel( 
+  leafletOutput("mymap", width = "100%", height = 1200) 
+  )
 ))
