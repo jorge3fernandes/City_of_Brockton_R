cdcf6c261ecdd487a9c6d6ac25645b6238fcf6bc
jorge3fernandes <jorge3fernandes@gmail.com>
2016-04-10 12:55:14 -0400

    r

--- Shiny_deploy.R
+++ Shiny_deploy.R
@@ -4,6 +4,6 @@ rsconnect::setAccountInfo(name = 'jomisilfe',
                           token = '92C0DD8F30100BB17590255422D1C8E3',
                           secret = 'COIobr9eqszqMv4IDD+XF3nq7mIC5y7x/jr2HGom')
 library(rsconnect)
-rsconnect::deployApp("C:/Users/jf82645/Desktop/Kaggle/Rbrockton/RBrockton")
+rsconnect::deployApp("/Users/legs_jorge/Desktop/RBrockton/RBrockton")
 
 
