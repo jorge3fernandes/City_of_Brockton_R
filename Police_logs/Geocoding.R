89055278bccb281f3993e03cb582baa9a9be3e69
jorge3fernandes <jorge3fernandes@gmail.com>
2016-04-18 13:18:50 -0400

    3

--- Police_logs/Geocoding.R
+++ Police_logs/Geocoding.R
@@ -1,9 +1,55 @@
-library(ggmap)
+Full_df <- read.csv("Full_df.csv")
 
-Full_df <- read.csv("full_bpd_calls.csv")
 Full_df$address <- as.character(Full_df$address)
 
-infile <- "input"
+Full_df$address[is.na(Full_df$address)] <- 0
+getGeoDetails <- function(address){ 
+  address <- as.character(address) 
+  #use the gecode function to query google servers
+  geo_reply = geocode(address, output='all', messaging=TRUE, override_limit=TRUE)
+  #now extract the bits that we need from the returned list
+  answer <- data.frame(lat=NA, 
+                       long=NA, 
+                       accuracy=NA, 
+                       formatted_address=NA, 
+                       address_type=NA, 
+                       status=NA)
+  answer$status <- geo_reply$status
+  
+  #if we are over the query limit - want to pause for an hour
+  while(geo_reply$status == "OVER_QUERY_LIMIT"){
+    print("OVER QUERY LIMIT - Pausing for 1 hour at:") 
+    time <- Sys.time()
+    print(as.character(time))
+    Sys.sleep(60*60)
+    geo_reply = geocode(address, output='all', messaging=TRUE, override_limit=TRUE)
+    answer$status <- geo_reply$status
+  }
+  
+  #return Na's if we didn't get a match:
+  if (geo_reply$status != "OK"){
+    return(answer)
+  }   
+  #else, extract what we need from the Google server reply into a dataframe:
+  answer$lat <- geo_reply$results[[1]]$geometry$location$lat
+  answer$long <- geo_reply$results[[1]]$geometry$location$lng   
+  if (length(geo_reply$results[[1]]$types) > 0){
+    answer$accuracy <- geo_reply$results[[1]]$types[[1]]
+  }
+  answer$address_type <- paste(geo_reply$results[[1]]$types, collapse=',')
+  answer$formatted_address <- geo_reply$results[[1]]$formatted_address
+  
+  return(answer)
+}
+
+#initialise a dataframe to hold the results
+geocoded <- data.frame()
+# find out where to start in the address list (if the script was interrupted before):
+startindex <- 1
+#if a temp file exists - load it up and count the rows!
+
+infile <- "input12"
+
 tempfilename <- paste0(infile, '_temp_geocoded.rds')
 if (file.exists(tempfilename)){
   print("Found temp file - resuming from index:")
@@ -24,3 +70,15 @@ for (ii in seq(startindex, length(Full_df$address))){
   #save temporary results as we are going along
   saveRDS(geocoded, tempfilename)
 }
+
+
+
+
+getGeoData <- function(location, api_key){
+  location <- gsub(' ','+',location)
+  geo_data <- getURL(paste("https://maps.googleapis.com/maps/api/geocode/json?address=",location,sprintf("&key=%s",api_key), sep=""))
+  geo_data <- fromJSON(geo_data)
+  return(geo_data$results[[1]]$geometry$location)
+}  
+
+
