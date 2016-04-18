ad8b6b9a842ca37df158eabaed68a22e1d46a777
jorge3fernandes <jorge3fernandes@gmail.com>
2016-04-18 10:32:24 -0400

    t

--- server.R
+++ server.R
@@ -22,14 +22,14 @@ shinyServer(function(input, output) {
   
   testdata <- subset(testdata, !is.na(Date))
   
-  test <- reactive({
-      testdata <- subset(testdata, as.Date(Date) >= input$date1[1] & as.Date(Date) <= input$date1[2])
+  testdata <- reactive({
+    testdata <- subset(testdata, as.Date(Date) >= input$date1[1] & as.Date(Date) <= input$date1[2])
     if(!is.null(input$Charges)){
       if(input$Charges == "All"){
         testdata
       }
       else{
-            testdata <- subset(testdata, charges %in% as.character(input$Charges))
+        testdata <- subset(testdata, charges %in% as.character(input$Charges))
           }
     }
     
@@ -57,6 +57,7 @@ shinyServer(function(input, output) {
   })
   output$mymap <- renderLeaflet({
     # define the leaflet map object
+    if(input$graph=='Clusters'){
     leaflet(data = test() ) %>% 
       addTiles() %>% 
       setView(-71.02016, 42.08667, zoom = 13) %>% addMarkers( ~long, ~lat, popup = paste("<b>","Call reason/Action: ","</b>", test()$call_reason_action,"<br>",
@@ -66,7 +67,19 @@ shinyServer(function(input, output) {
                                                               "<b>","Arrested: ","</b>", test()$Arrested, "<br>",
                                                               "<b>","Arr/Summ Address: ","</b>", test()$Occurrence_location, "<br>",
                                                               "<b>","Age: ","</b>", test()$Age,"<br>",
-                                                              "<b>","Date: ","</b>",test()$Date),clusterOptions = markerClusterOptions(zoomToBoundsOnClick = TRUE))
+                                                              "<b>","Date: ","</b>",test()$Date),clusterOptions = markerClusterOptions(zoomToBoundsOnClick = TRUE, removeOutsideVisibleBounds = TRUE))}
+    else{
+      leaflet(data = test() ) %>% 
+        addTiles() %>% 
+        setView(-71.02016, 42.08667, zoom = 13) %>% addMarkers( ~long, ~lat, popup = paste("<b>","Call reason/Action: ","</b>", test()$call_reason_action,"<br>",
+                                                                                           "<b>","Occurrence Address: ","</b>", test()$formatted_address, "<br>",
+                                                                                           "<b>","Charges: ","</b>", test()$charges, "<br>",
+                                                                                           "<b>","Summoned:","</b>", test()$Summons, "<br>",
+                                                                                           "<b>","Arrested: ","</b>", test()$Arrested, "<br>",
+                                                                                           "<b>","Arr/Summ Address: ","</b>", test()$Occurrence_location, "<br>",
+                                                                                           "<b>","Age: ","</b>", test()$Age,"<br>",
+                                                                                           "<b>","Date: ","</b>",test()$Date))
+    }
     
   })
   
