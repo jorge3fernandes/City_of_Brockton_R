92f6931db73ce6d3ad8de370a409267a24a8da64
jorge3fernandes <jorge3fernandes@gmail.com>
2016-12-07 21:46:26 -0500

    1207

--- Police_logs/Analysis.R
+++ Police_logs/Analysis.R
@@ -1,1 +1,5 @@
-crimes <- subset(Full, )
+# Cleanning the data for analysis
+getwd()
+
+setwd("/Users/legs_jorge/Desktop/RBrockton/RBrockton/Police_logs")
+clean_data <- read.csv("full_bpd_calls.csv")
