bb7dc16f0d6720991cfde448622240d1f2efed80
jorge3fernandes <jorge3fernandes@gmail.com>
2017-11-26 11:52:39 -0500

    Clean-up
    Deleted and organized download PDFs

--- Shinybundle 2/Shiny_deploy.R
+++
@@ -1,9 +0,0 @@
-install.packages('devtools')
-devtools::install_github('rstudio/rsconnect')
-rsconnect::setAccountInfo(name = 'jomisilfe',
-                          token = '92C0DD8F30100BB17590255422D1C8E3',
-                          secret = 'COIobr9eqszqMv4IDD+XF3nq7mIC5y7x/jr2HGom')
-library(rsconnect)
-rsconnect::deployApp("C:/Users/jf82645/Desktop/Kaggle/Rbrockton/RBrockton")
-
-
