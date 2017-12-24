63c5dda15f2e122b90e7274a8fb6ef3a9e146e26
jorge3fernandes <jorge3fernandes@gmail.com>
2017-12-24 13:53:14 -0500

    Multiple Changes. See details
     - Added relative root directory to crawler.r
     - deleted duplicate PDFs.
     - Updated crawler.r use two
        functions instead of multiple steps
     - Deleted duplicate files under
        crawler_result_Conversion folder
    
    
    Former-commit-id: 8b53e089254de63ce2cfe31540418d1b5bb639b7

--- src/crawler.R
+++ src/crawler.R
@@ -6,33 +6,68 @@ library(here) # For relative working directory. Looks for where there is a file 
 
 # The root directory will automatically be detected if .Rproj or .here file in detect
 
-crawlerResultPath <- file.path(getwd(),                       #gets the working root directory  
-                               'crawler_result_Conversion',   # folder where we want to  save the PDFs and 
+crawlerResultPath <- file.path(getwd(),                       # gets the working root directory  
+                               'crawler_result_Conversion',   # folder where we want to save the PDFs and 
                                fsep = .Platform$file.sep)     # makes sure the path is platform agnostic
+setwd(crawlerResultPath) # ensure the files are being downloaded to the right folder
 
 firstPg <- "http://www.brocktonpolice.com/category/police-log/" 
+PgPrefix <- "http://www.brocktonpolice.com/category/police-log/page/" # base url to add page numbers
 
-# get a list of pdf (logs) available for downloading
-homepageLinks <- read_html(firstPg) %>% 
-  html_nodes("a") %>%       # find all links
-  html_attr("href") %>%     # get the url
-  str_subset("\\.pdf")
+GetAllLinks <- function(DispLogHomePg, AddtnlPgPrefix){
+  # Gather all the links to the PDF dispatch log
+  # Args: 
+  # DispLogHomePg: The first page containing the links to the PDFs 
+  # AddtnlPgPrefix: A prefix to the subsequent pages. i.e. http://www.brocktonpolice.com/category/police-log/page/ 
+  # Returns:
+  # A list of links
+  
+  # get a list of pdf (logs) available for downloading
+  DispLogHomePg <- read_html(firstPg) %>% 
+    html_nodes("a") %>%       # find all links
+    html_attr("href") %>%     # get the url
+    str_subset("\\.pdf")
+  # getting the number of pages in the website to cycle through
+  pageNum <- read_html(firstPg) %>% 
+    html_nodes(".page-numbers") %>%                    # find all links for the pages
+    html_attr("href") %>%                           # extracts the urls
+    .[!is.na(.)] %>% 
+    str_extract_all("page/[[:digit:]]{1}") %>%
+    str_replace_all("page/","") %>%
+    max(unique(.))
+  p <- 2 # start on page 2 since we already scraped the first page
+  allLinks <- DispLogHomePg # initializing the list with the homepage links
+  
+  while (p <= pageNum) {
+    url <- paste0(AddtnlPgPrefix,p)
+    pageLinks <- read_html(url) %>% 
+      html_nodes("a") %>%       # find all nodes on the page
+      html_attr("href") %>%     # get the urls
+      str_subset("\\.pdf") 
+    allLinks <- append(allLinks, pageLinks)
+    p <- p + 1
+  }
+  return(allLinks)
+}
 
-DownloadEverythingOnFirstPage <- function(listOfLinks, savePath){
+AllLinks <- GetAllLinks(firstPg,PgPrefix)
+
+DownloadAllLinks <- function(listOfLinks){
   # Description: 
     # download everything on the first page for each links in the listOfLinks
   # Arguments: 
     # listOfLinks: a vector of links 
     # savePath: path to save the results in
-
+    
   # Gets the time period covered in the log
   regexDate <- "Dispatch.*Thru: \\d{2}/\\d{2}/\\d{4}"
-
-  for (i in seq(length(listOfLinks))) {
+  lengthAllLinks <- length(listOfLinks)
+  # TODO(Jorge3fFernandes): Use Apply family to get rif of for loops
+  for (i in seq(lengthAllLinks)) {
     tryCatch({
-      print(paste0("Working on file ", i, "/",
-                   length(listOfLinks), " on page 1"))
-      homepageLinks[i] %>%  # find those that end in .pdf
+      print(paste0("Working on link ", i, "/",
+                   lengthAllLinks))
+      listOfLinks[i] %>%  
         download.file(paste0("file",i ,".pdf"),
                       mode = "wb")  # Apply the download function
 
@@ -42,62 +77,17 @@ DownloadEverythingOnFirstPage <- function(listOfLinks, savePath){
               str_replace_all("/", "_") %>% 
               str_replace_all(" ", "") %>%
               str_replace_all(":", "")
-      name_pdf <- paste0(savePath, date, ".pdf")
-      name_txt <- paste0(savePath, date, ".txt")
+      name_pdf <- paste0(date, ".pdf")
+      name_txt <- paste0(date, ".txt")
       file.rename(paste0("file", i, ".pdf"),name_pdf)
       write(pdf_txt, name_txt)
     },error = function(e){
     })
   } 
-  print("Finished Page 1")
 }
 
-DownloadEverythingOnFirstPage(homepageLinks,crawlerResultPath)
+DownloadAllLinks(AllLinks)
 
-#moving to the next pages 
-
-# getting the number of pages in the website to cycle through
-pagenum <- read_html(firstPg) %>% 
-  html_nodes(".page-numbers") %>%                    # find all links for the pages
-  html_attr("href") %>%                           # extracts the urls
-  .[!is.na(.)] %>% 
-  str_extract_all("page/[[:digit:]]{1}") %>%
-  str_replace_all("page/","") %>%
-  max(unique(.))
-p = 2
-root = "http://www.brocktonpolice.com/category/police-log/page/" #base url to add page numbers
-while (p <= pagenum) {
-  url = paste0(root,p)
-  page_links <- read_html(url) %>% 
-    html_nodes("a") %>%       # find all nodes on the page
-    html_attr("href") %>%     # get the urls
-    str_subset("\\.pdf")      # get all the
-  i = 1
-  while (i <= length(page_links)) { 
-    # find those that end in .pdf
-    print(paste0("working on file ", i, "/",length(page_links), " on page ", p))
-    tryCatch({
-      page_links[i] %>%
-        download.file(paste0("file",i,".pdf"), mode = "wb") # Apply the download function
-      
-      #renaming the files as its being downloaded
-      pdf_txt <- pdf_text(paste0("file", i, ".pdf"))
-      txt <- str_c(pdf_txt, collapse = "\n") # this may not be needed actually
-      date <- str_extract(txt, regexDate) %>%
-        str_replace_all("/", "_") %>% 
-        str_replace_all(" ", "") %>%
-        str_replace_all(":", "")
-      name_pdf <- paste0(savePath, date, ".pdf")
-      name_txt <- paste0(savePath, date, ".txt")
-      file.rename(paste0("file", i, ".pdf"),name_pdf)
-      write(pdf_txt, name_txt)
-    },error = function(e){}
-    )
-    i = i + 1
-  } 
-  print(paste0("Finished Page ", p))
-  p = p + 1
-}
 
 # test to see that we have the same number of PDF as TXT files
 # this somewhat ensures that all the PDF have been converted to txt
