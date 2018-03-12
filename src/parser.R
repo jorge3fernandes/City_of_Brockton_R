8b93cc9553ad3209df81493a333eb20f00a989d4
jorge3fernandes <jorge3fernandes@gmail.com>
2018-03-11 23:54:24 -0400

    Changed all the loops to start using lapply
    I still need to fix an issue when extracting dates.

--- src/parser.R
+++ src/parser.R
@@ -2,6 +2,158 @@ library(stringr)   # For text manipulation
 library(testthat)  # For unittesting: test_that
 library(pdftools)  # For PDF to Text Conversion
 
+# List of RegEx
+callSplitRegEx <- "###CallSplit###"
+timeOfDayRegEx <- "       [[:digit:]]{4}"
+dateRegEx <- "\\d{2}/\\d{2}/\\d{4}"
+callTakerRegEx <- "Call Taker:.*\n"
+dispAddressRegEx <- "Location/Address:.*\n|Vicinity of:.*\n"
+policeOff1RegEx <- "(?s)Location\\/Address:[^\n]*\\R(.*)|(?s)Vicinity of:[^\n]*\\R(.*)"
+policeOff2RegEx <- "ID:.*\n|Patrolman.*\n"
+callReasonActionRegEx <- "       [[:digit:]]{4}.*\n"
+referToArrestRegEx <- "Refer To Arrest:.*\n"
+referToSummonRegEx <- "Refer To Summons:.*\n"
+arrestRegEx <- "          Arrest:    .*\n"
+summonRegEx <- "         Summons:    .*\n"
+ageRegEx <- "Age:.*\n"
+indivAddressRegEx <- "         Address:    .*\n"
+chargesRegEx <- "Charges:    .*\n"
+responseTimeRegEx <- "Arvd.*\n"
+daySplitRegEx <- "###DaySplit###" # marks where day begins and is used to split the texts
+callSplitRegEx <- "###CallSplit###" # marks where call begins and is used to split the texts
+logDateRegEx <- "For Date:.*[[:digit:]]{4} "
+logtimeOfDayRegEx <- "        [[:digit:]]{4}  "
+
+DateFlag <- function(text){
+  
+  if (str_detect(text,logDateRegEx)) { 
+    text <- paste0(daySplitRegEx,text ) # flags where the day starts
+  }
+  return(text)
+}
+CallFlag <- function(text){
+  if (str_detect(text,logtimeOfDayRegEx)) { 
+    text <- paste0(callSplitRegEx,text ) # flags where the call starts
+  }
+  return(text)
+}
+callParser <- function(txtparts){
+  
+  
+  # List of RegEx
+  callSplitRegEx <- "###CallSplit###"
+  timeOfDayRegEx <- "       [[:digit:]]{4}"
+  dateRegEx <- "\\d{2}/\\d{2}/\\d{4}"
+  callTakerRegEx <- "Call Taker:.*\n"
+  dispAddressRegEx <- "Location/Address:.*\n|Vicinity of:.*\n"
+  policeOff1RegEx <- "(?s)Location\\/Address:[^\n]*\\R(.*)|(?s)Vicinity of:[^\n]*\\R(.*)"
+  policeOff2RegEx <- "ID:.*\n|Patrolman.*\n"
+  callReasonActionRegEx <- "       [[:digit:]]{4}.*\n"
+  referToArrestRegEx <- "Refer To Arrest:.*\n"
+  referToSummonRegEx <- "Refer To Summons:.*\n"
+  arrestRegEx <- "          Arrest:    .*\n"
+  summonRegEx <- "         Summons:    .*\n"
+  ageRegEx <- "Age:.*\n"
+  indivAddressRegEx <- "         Address:    .*\n"
+  chargesRegEx <- "Charges:    .*\n"
+  responseTimeRegEx <- "Arvd.*\n"
+  daySplitRegEx <- "###DaySplit###" # marks where day begins and is used to split the texts
+  callSplitRegEx <- "###CallSplit###" # marks where call begins and is used to split the texts
+  logDateRegEx <- "For Date:.*[[:digit:]]{4} "
+  logtimeOfDayRegEx <- "        [[:digit:]]{4}  "
+  timeOfDayRegEx <- "       [[:digit:]]{4}"
+  
+  
+  #extracting specific fields
+  timeOfDay <- str_extract_all(txtparts,timeOfDayRegEx) %>% 
+    sub("(..)$", ":\\1", .)
+  logDate <- str_extract(txtparts, dateRegEx)[1] %>%
+    rep(length(timeOfDay))
+  timeStamp <- paste0(logDate," ",timeOfDay)
+  callTaker <- str_extract_all(txtparts, callTakerRegEx) %>% 
+    str_replace_all("Call Taker:|\n","") 
+  dispAddress <- str_extract_all(txtparts, dispAddressRegEx) %>% 
+    str_replace_all("Location/Address:|Vicinity of:|\n","") 
+  policeOff <- str_extract_all(txtparts, policeOff1RegEx) %>% 
+    str_extract_all(policeOff2RegEx) %>% 
+    str_replace_all("ID:    |Patrolman ", "")
+  callReasonAction <- str_extract_all(txtparts, callReasonActionRegEx) %>% 
+    str_replace_all("[[:digit:]]{4}","")
+  referToArrest <- str_extract_all(txtparts, referToArrestRegEx) %>% 
+    str_replace_all("Refer To Arrest:","")
+  referToSummon <- str_extract_all(txtparts, referToSummonRegEx) %>%
+    str_replace_all("Refer To Summons:|\n","") 
+  summons <- str_extract_all(txtparts, summonRegEx) %>% 
+    str_replace_all("Summons:","") 
+  arrest <- str_extract_all(txtparts, arrestRegEx) %>%
+    str_replace_all("Arrest:","")
+  age <- str_extract_all(txtparts,ageRegEx) %>% 
+    str_replace_all("Age:","") 
+  indivAddress <- str_extract_all(txtparts,indivAddressRegEx) %>% 
+    str_replace_all("Address:","") 
+  charges <- str_extract_all(txtparts,chargesRegEx) %>% 
+    str_replace_all("Charges:","")
+  responseTime <- str_extract_all(txtparts,responseTimeRegEx) %>%
+    str_replace_all("c\\(\\\"    |\\\n\"|\"","") 
+  
+  df <- data.frame(Time = timeOfDay, Date = logDate, timeStamp = timeStamp, operator = callTaker, 
+                   dispAddress = dispAddress, officer = policeOff, callReasonAction = callReasonAction, 
+                   referToArrest = referToArrest, referToSummon = referToSummon, summons = summons, 
+                   arrest = arrest, age = age, indivAddress = indivAddress, charges = charges, responseTime = responseTime ) 
+  return(df)
+}
+
+CallInfoExtractor <- function(dayLog){
+  # Reads in a vector of daily logs and extracts individual call info
+  #
+  # Args:
+  #   dayLog: daily log in .txt format
+  #
+  # Returns:
+  #   a data frame
+  
+  
+  do.call("rbind", apply(txtparts, 1, callParser))
+    
+  
+}
+pdfToTable <- function(pdfList){
+  #reads in a list of PDF files from a folder or the web and transforms them into 
+  #one text file where each individual line is an element of the list
+  #
+  #Args:
+  # textList: List of PDF files
+  #
+  #Returns: Returns one text file broken down by line
+  #
+  textFile <- pdf_text(AllLinks[5]) # converting PDF to text
+  
+  pages <- textFile %>% 
+    str_split("[\r\n]") %>% # Spliting the line into individual list element so we can later flag for when the day and call started
+    unlist() 
+  
+  text <- lapply(pages,DateFlag) %>% # Flagging where each day starts
+    unlist() %>% 
+    lapply(CallFlag) %>% # Flagging where each call starts
+    unlist() %>% 
+    str_c(collapse = "\n") %>%
+    str_split(.,regex(daySplitRegEx)) %>% # uses daystart flag to split into daily logs
+    unlist()
+  
+  txtparts  <-  str_split(text, callSplitRegEx) %>% # Splits a single day into multiple elements(Individual calls)
+                  unlist()
+  
+  dataFinal <- do.call("rbind", lapply(txtparts, callParser))
+}
+
+
+
+
+
+
+
+
+
 # TODO(Jorge3fFernandes): Use Apply family to get rif of for loops
 
 crawlerResultPath <- "./crawler_result_Conversion" # text and pdf location
@@ -38,11 +190,12 @@ oneText <- function(pdfList){
 
 #Can either choose a list of PDFs from a directory or just use the links (AllLinks) from crawler.R.
 
-fullTxt <- oneText(listAllPDFs)
+fullTxt <- oneText(AllLinks)
 
 # #file location
 # listOfTxtFiles <- list.files(crawlerResultPath, pattern = ".txt", full.names = TRUE)
 
+startTime <- proc.time()[3]
 GetIndivDays <- function(text){
   
   # Breaks the text file into individual days and creates a lists of daily logs.
@@ -57,7 +210,7 @@ GetIndivDays <- function(text){
   callSplitRegEx <- "###CallSplit###" # marks where call begins and is used to split the texts
   logDateRegEx <- "For Date:.*[[:digit:]]{4} "
   logtimeOfDayRegEx <- "        [[:digit:]]{4}  "
-  startTime <- proc.time()[3]
+  
   
   
   for (i in seq_along(text)) { 
@@ -74,86 +227,40 @@ GetIndivDays <- function(text){
   dayVector <-  str_c(text, collapse = "\n") %>%
     str_split(.,regex(daySplitRegEx)) %>% # uses daystart flag to split into daily logs
     unlist()
+  
   print(paste("DONE! - Time lapsed: ", proc.time()[3] - startTime))
   return(dayVector)
 }
+time1 <- proc.time()[3] - startTime
 
-allDays <- GetIndivDays(fullTxt)
-
-CallInfoExtractor <- function(dayLog){
-  # Reads in a vector of daily logs and extracts individual call info
+startTime <- proc.time()[3]
+GetIndivDays_dplyr <- function(text){
+  
+  # Breaks the text file into individual days and creates a lists of daily logs.
   #
   # Args:
-  #   dayLog: daily log in .txt format
+  #   pdfTxt: a list of text files.
   #
   # Returns:
-  #   a data frame
- 
-  # List of RegEx
-  callSplitRegEx <- "###CallSplit###"
-  timeOfDayRegEx <- "       [[:digit:]]{4}"
-  dateRegEx <- "\\d{2}/\\d{2}/\\d{4}"
-  callTakerRegEx <- "Call Taker:.*\n"
-  dispAddressRegEx <- "Location/Address:.*\n|Vicinity of:.*\n"
-  policeOff1RegEx <- "(?s)Location\\/Address:[^\n]*\\R(.*)|(?s)Vicinity of:[^\n]*\\R(.*)"
-  policeOff2RegEx <- "ID:.*\n|Patrolman.*\n"
-  callReasonActionRegEx <- "       [[:digit:]]{4}.*\n"
-  referToArrestRegEx <- "Refer To Arrest:.*\n"
-  referToSummonRegEx <- "Refer To Summons:.*\n"
-  arrestRegEx <- "          Arrest:    .*\n"
-  summonRegEx <- "         Summons:    .*\n"
-  ageRegEx <- "Age:.*\n"
-  indivAddressRegEx <- "         Address:    .*\n"
-  chargesRegEx <- "Charges:    .*\n"
-  responseTimeRegEx <- "Arvd.*\n"
+  #   a vector/list of daily dispatch log.
   
   
-  for (i in seq_along(dayLog)) {
-    
-    txtparts  <-  unlist(str_split(dayLog[i], callSplitRegEx))
-    
-    #extracting specific fields
-    timeOfDay <- str_extract_all(txtparts,timeOfDayRegEx) %>% 
-      sub("(..)$", ":\\1", .)
-    logDate <- str_extract(txtparts, dateRegEx)[1] %>%
-      rep(length(timeOfDay))
-    timeStamp <- paste0(logDate," ",timeOfDay)
-    callTaker <- str_extract_all(txtparts, callTakerRegEx) %>% 
-      str_replace_all("Call Taker:|\n","") 
-    dispAddress <- str_extract_all(txtparts, dispAddressRegEx) %>% 
-      str_replace_all("Location/Address:|Vicinity of:|\n","") 
-    policeOff <- str_extract_all(txtparts, policeOff1RegEx) %>% 
-      str_extract_all(policeOff2RegEx) %>% 
-      str_replace_all("ID:    |Patrolman ", "")
-    
-    callReasonAction <- str_extract_all(txtparts, callReasonActionRegEx) %>% 
-      str_replace_all("[[:digit:]]{4}","")
-    referToArrest <- str_extract_all(txtparts, referToArrestRegEx) %>% 
-      str_replace_all("Refer To Arrest:","")
-    referToSummon <- str_extract_all(txtparts, referToSummonRegEx) %>%
-      str_replace_all("Refer To Summons:|\n","") 
-    summons <- str_extract_all(txtparts, summonRegEx) %>% 
-      str_replace_all("Summons:","") 
-    arrest <- str_extract_all(txtparts, arrestRegEx) %>%
-      str_replace_all("Arrest:","")
-    age <- str_extract_all(txtparts,ageRegEx) %>% 
-      str_replace_all("Age:","") 
-    indivAddress <- str_extract_all(txtparts,indivAddressRegEx) %>% 
-      str_replace_all("Address:","") 
-    charges <- str_extract_all(txtparts,chargesRegEx) %>% 
-      str_replace_all("Charges:","")
-    responseTime <- str_extract_all(txtparts,responseTimeRegEx) %>%
-      str_replace_all("c\\(\\\"    |\\\n\"|\"","") 
-    
-    df <- cbind(timeOfDay, logDate, timeStamp, callTaker, dispAddress,
-                policeOff, callReasonAction, referToArrest, referToSummon, 
-                summons, arrest, age, indivAddress, charges, responseTime ) %>% 
-      data.frame(stringsAsFactors = FALSE) 
-  }
   
-  return(df)  
   
+  
+  
+  
+  dayVector <-  str_c(text, collapse = "\n") %>%
+    str_split(.,regex(daySplitRegEx)) %>% # uses daystart flag to split into daily logs
+    unlist()
+  
+  print(paste("DONE! - Time lapsed: ", proc.time()[3] - startTime))
+  return(dayVector)
 }
+time2 <- proc.time()[3] - startTime
+
+
+allDays <- GetIndivDays(fullTxt)
 
 
 cmpltDf <- NULL
@@ -178,5 +285,4 @@ test_that("Test01: verify the parser is working on test file",{
   df_test <- read.csv('./crawler_result_Conversion/test_df.csv')
   expect_that(testCase, function(x){all.equal(x, df_test)})
 })
-
 
